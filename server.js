84c0c5f9ffec9a7bc165ee5c53898b14b77699f0
Zajroid <zajroid@gmail.com>
2022-08-12 15:32:01 +0300

    chat example

--- server.js
+++ server.js
@@ -10,18 +10,17 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
   });
 });
+
+
 
 server.listen(PORT, () => {
   console.log(`Server been started on port: ${PORT}`);
